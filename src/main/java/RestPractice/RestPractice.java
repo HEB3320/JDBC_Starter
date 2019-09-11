a03aaf9f3d084c8d12c24018a26ef87b4dabc10c
cybertek <github@cybertekschool.com>
2019-09-10 20:41:20 -0400

    added simple junit test with junit 4 dependency

---
+++ src/main/java/RestPractice/RestPractice.java
@@ -0,0 +1,18 @@
+package RestPractice;
+
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class RestPractice {
+
+    @Test
+    public void test1(){
+
+
+
+
+    }
+
+
+}
