9b2d113dddd1edf1ba914f0017375f975805816c
cybertek <github@cybertekschool.com>
2019-09-14 16:56:18 -0400

    hamcrest matcher with Rest Assured

--- src/test/java/RestPractice/HamCrestLibrary.java
+++ src/test/java/RestPractice/HamCrestLibrary.java
@@ -1,9 +1,79 @@
 package RestPractice;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class HamCrestLibrary {
 
+    @BeforeClass
+    public static void setUp() {
+        RestAssured.baseURI = "http://3.89.115.0";
+        RestAssured.port = 8000;
+        RestAssured.basePath = "/api";
+        // above will generate a BASE REQUEST URL OF http://52.23.254.102:8000/api
+    }
 
 
+
+    @Test
+    public void DoingAssertionWithHamcrest_ForSpartan(){
+
+
+        //Response response= given().pathParam("my_id",3).get("/spartans/{my_id}");
+        //response.prettyPrint();
+        given()
+                .pathParam("my_id",3).
+        when()
+                .get("/spartans/{my_id}").
+        then()
+                .assertThat()
+                .statusCode(equalTo(200))
+                .contentType(ContentType.JSON)
+                .body("id",equalTo(3))
+                .body("gender", equalToIgnoringCase("male"))
+                .body("phone",equalTo("6105035231"));
+
+
+
+    }
+
+
+
+    // hamcrest is a testing library to provide many built in testing methods
+    // that comes from hamcrest Matcher
+    @Test
+    public void Calculation_Test(){
+
+        int a = 10 ,b = 20 ;
+
+        assertEquals(30, a+b) ;
+
+        assertThat(30, equalTo(a+b));
+        assertThat(20, greaterThan(a+b) );
+
+    }
+
+
+
+    @Test
+    public void arrayTest(){
+
+        int[] nums = {1,4,6,7,8} ;
+
+        //Matchers.contains()
+       //assertThat(nums  , hasItemInArray(1));
+       // assertThat(1 ,hasItemInArray(1) );
+
+
+    }
 
 
 }
