e9b4ed95c4a0372bdf21c53ac604f820588a82b3
cybertek <github@cybertekschool.com>
2019-09-15 16:50:51 -0400

    random data generation using post

--- src/test/java/RestPractice/Full_Cycle_Test.java
+++ src/test/java/RestPractice/Full_Cycle_Test.java
@@ -4,31 +4,92 @@ import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Full_Cycle_Test extends TestBase{
 
 
     @Test
-    public void getSpartan_Test(){
+    public void getSpartanAndDelete_Test(){
+        // calling utility to create new data
+        int spartanID = createRandomSpartan();
+        System.out.println(spartanID);
+
+        //  updating the data using put request
+        given()
+                .log().all()
+                .pathParam("id",spartanID)
+                .body(new Spartan("Zeynep","Female",1231231231)).
+        when()
+                .put("/spartans/{id}").
+        then()
+                .log().all()
+                .statusCode(204)
+                ;
 
 
-        createRandomSpartan();
+
+
+
+        //  verifying the updated data using get request
+        given()
+                .log().all()
+                .pathParam("id",spartanID).
+        when()
+                .get("/spartans/{id}").
+        then()
+                .assertThat()
+                .and()
+                .statusCode(200)
+                .and()
+                .body("id",is(spartanID))
+                .body("name",is("Zeynep"))
+                .body("gender",is("Female"))
+                .body("phone",hasToString("1231231231"))
+                .and()
+                .header("content-type",is("application/json;charset=UTF-8"))
+
+        ;
+    // deleting the data after verification
+        given()
+                .log().all().
+        when()
+                .delete("/spartans/"+spartanID).
+        then()
+                .log().all()
+                .statusCode(204)
+                // do not provide json path for empty body
+                .body(blankOrNullString())
+                ;
+
+        // making sure the deleted data does not exists
+        given()
+                .log().all().
+        when()
+                .get("/spartans/"+spartanID).
+        then()
+                .statusCode(404)
+        ;
 
     }
 
-
+    /*
+    This method is creating random Spartan data by using Post request
+    It use faker to get random name , gender , phone number
+    eventually return the id of new resource created in the server
+     */
     public int createRandomSpartan(){
-        Faker faker = new Faker();
 
+        Faker faker = new Faker();
 
         int randomIndex = faker.number().numberBetween(0,2);
         String[] genders = {"Male","Female"};
         String randomGender = genders[randomIndex];
 
         Spartan spartan = new Spartan(faker.name().firstName(),
-                            "Male",
-                            Long.parseLong(faker.number().digits(10) ) );
+                            randomGender,
+                            Long.parseLong(faker.number().digits(11) ) );
 
 
        return given()
@@ -37,6 +98,7 @@ public class Full_Cycle_Test extends TestBase{
                     .body(spartan).
                when()
                     .post("/spartans")
+                    .prettyPeek()
                     .jsonPath()
                     .getInt("data.id") ;
 
