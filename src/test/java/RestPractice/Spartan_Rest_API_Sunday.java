fcfd21b87963d903913aa7aca93729216b6efced
cybertek <github@cybertekschool.com>
2019-09-15 11:53:53 -0400

    chaining restassured library methods and using hamcrest for assertion

--- src/test/java/RestPractice/Spartan_Rest_API_Sunday.java
+++ src/test/java/RestPractice/Spartan_Rest_API_Sunday.java
@@ -1,7 +1,9 @@
 package RestPractice;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import utils.ConfigurationReader;
 
 
@@ -19,5 +21,28 @@ public class Spartan_Rest_API_Sunday {
         RestAssured.basePath = ConfigurationReader.getProperty("spartan.base_path");
         // above will generate a BASE REQUEST URL OF http://52.23.254.102:8000/api
     }
+
+    @Test
+    public void All_Spartan_With_Size_And_Items_Test(){
+
+        given()
+                .accept(ContentType.JSON).
+        when()
+                .get("/spartans").prettyPeek().
+        then()
+                .statusCode(200)
+                .assertThat()
+                .body("[0].name",equalTo("Nels"))
+                //.body("name",hasSize(104))
+                .body("[1].gender",is("Male"))
+                .header("Transfer-Encoding","chunked")
+                .header("Date", notNullValue() )
+
+
+        ;
+
+
+    }
+
 
 }
