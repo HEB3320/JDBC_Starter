92c402a8b9fe5ed61a54e72104bbc59606cf6917
cybertek <github@cybertekschool.com>
2019-09-18 20:45:15 -0400

    added cucumber support

--- src/test/java/step_definitions/MyStepdefs.java
+++ src/test/java/step_definitions/MyStepdefs.java
@@ -6,19 +6,19 @@ public class MyStepdefs {
     @Given("credentials with USER_role is provided")
     public void credentials_with_USER_role_is_provided() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("given part");
     }
 
     @When("user try to send get request on {string}")
     public void user_try_to_send_get_request_on(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("when part");
     }
 
     @Then("user should get status code {int}")
     public void user_should_get_status_code(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("then part");
     }
 
 
